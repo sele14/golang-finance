11dc12fbc4f705badbfe8023bcb9c2182f2d27b3
sele14 <eirik.selander@gmail.com>
2022-04-18 22:05:14 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func readCsvFile(filePath string) []float64 {
+    f, err := os.Open(filePath)
+    if err != nil {
+        log.Fatal("Unable to read input file " + filePath, err)
+    }
+    defer f.Close()
+
+    csvReader := csv.NewReader(f)
+    records, err := csvReader.ReadAll()
+
+    var priceList []float64
+
+    for i, row := range records {
+        if i > 0 { // drop header
+            priceList = append(priceList, s2f(row[0]))
+        }
+    }
+
+    // ignore header
+    if err != nil {
+        log.Fatal("Unable to parse file as CSV for " + filePath, err)
+    }
+
+    return priceList
+}
+
+// s2f converts string to float64
+func s2f(str string) float64 {
+    f, _ := strconv.ParseFloat(str, 64)
+    return f
+}
+
+func main() {
+    prices := readCsvFile("prices1.csv")
+    fmt.Println(prices)
+}
